4446e1f641d9ae3d0c7bca3182f80ac896f536eb
Nikita <Kulacnikita@ya.ru>
2020-10-26 16:26:37 +0300

    First four locations

---
+++ Arctic-Isolation-Game/Scripts/Player.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+var motion = Vector2()
+
+var velocity_multiplier = 1
+
+const SPEED = 20
+const MAX_SPEED = 200
+const FRICTION = 0.1
+
+
+func _physics_process(delta):
+	update_movement()
+	move_and_slide(motion * velocity_multiplier)
+
+
+func update_movement():
+	look_at(get_global_mouse_position())
+	
+	if Input.is_action_pressed("move_down") and not Input.is_action_pressed("move_up"):
+		motion.y = clamp(motion.y + SPEED, 0, MAX_SPEED)
+	elif Input.is_action_pressed("move_up") and not Input.is_action_pressed("move_down"):
+		motion.y = clamp(motion.y - SPEED, -MAX_SPEED, 0)
+	else:
+		motion.y = lerp(motion.y, 0, FRICTION)
+
+	if Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
+		motion.x = clamp(motion.x - SPEED, -MAX_SPEED, 0)
+	elif Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
+		motion.x = clamp(motion.x + SPEED, 0, MAX_SPEED)
+	else:
+		motion.x = lerp(motion.x, 0, FRICTION)
+	
+	
