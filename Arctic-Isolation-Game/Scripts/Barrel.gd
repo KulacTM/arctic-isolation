9eba2af99d710e1b2b0cceaef685bc1dbdf1e4e2
Nikita <Kulacnikita@ya.ru>
2020-10-29 19:20:16 +0300

    работаем над картой

--- Arctic-Isolation-Game/Scripts/Barrel.gd
+++ Arctic-Isolation-Game/Scripts/Barrel.gd
@@ -4,15 +4,18 @@ var can_light = false #if player can light a fire
 var is_fired = false #if fire is already fired
 
 func _on_Barrel_body_entered(body):
+	$Press_E.show()
 	if Inventory.has_lighter:
 		can_light = true
 	if is_fired:
 		Cold.multiplier = -300
 		can_light = false
+		$Press_E.hide()
 
 func _on_Barrel_body_exited(body):
 	can_light = false
 	Cold.multiplier = Cold.default_multiplier
+	$Press_E.hide()
 
 
 
@@ -20,6 +23,7 @@ func _process(delta):
 	if can_light and Input.is_action_just_pressed("action"):
 		$Sprite.texture = load("res://GFX/textures/bochka/bochka2.png")
 		$Light2D.enabled = true
+		$Press_E.hide()
 		$BonfireSound.playing = true
 		Cold.multiplier = -50
 		$Timer.start()
@@ -34,6 +38,7 @@ func _on_Timer_timeout():
 	$Light2D.enabled = false
 	is_fired = false
 	can_light = true
+	$Press_E.show()
 
 
 
